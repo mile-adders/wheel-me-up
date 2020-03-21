2cc5a27d791ff96be528b13e7c9733f003bc3165
mai <mai.yusuf28@yahoo.com>
2020-03-21 13:47:21 +0200

    test

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 'use strict';
 
-require('dotenv').config();
-const server = require('./auth/server.js');
-const MONGODB_URI = 'mongodb://localhost:27017/car-db';
 
 
-// Start up DB Server
-const mongoose = require('mongoose');
-const options = {
-  useNewUrlParser:true,
+
+
+
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const mongooseOptions = {
+  useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 };
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
+let Family = mongoose.model("Family", { name: String, position: String });
+//client
+app.get("/", (req, res) => {
+  // const m = new Family({ name: 'mahmoud',position:"Instructor" });
+  // m.save().then(() => res.send('meow'));
+  Family.find({}).then(data => {
+    res.json(data);
+  });
+});
+app.listen(PORT, () => console.log("listing on port", PORT));
 
-mongoose.connect(process.env.MONGODB_URI, options);
-server.start(process.env.PORT);
+// require('dotenv').config();
+// const server = require('./auth/server.js');
+// const MONGODB_URI = 'mongodb://localhost:27017/car-db';
+
+
+// // Start up DB Server
+// const mongoose = require('mongoose');
+// const options = {
+//   useNewUrlParser:true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// };
+
+// mongoose.connect(process.env.MONGODB_URI, options);
+// server.start(process.env.PORT);
