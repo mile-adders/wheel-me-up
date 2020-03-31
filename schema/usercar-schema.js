c450e379219051d394fd731888bb95454bf846b5
mai <mai.yusuf28@yahoo.com>
2020-03-31 16:25:46 +0300

    change of the type in the schema

--- schema/usercar-schema.js
+++ schema/usercar-schema.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 const userCar = mongoose.Schema({
   pickupLocation :{ type : String , require:true },
   dateAvailable :{ type : Date , require:true },
-  dropOffDate :{ type : Date , require :true} ,
   carType :{ type :String , require:true  } ,
+  dropOffDate :{ type : Date , require :true} ,
  } ,
  {toObject: { virtuals: true}, toJSON: { virtuals: true }});
 
